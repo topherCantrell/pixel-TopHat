627ff39c3152cacc2af8b2019d1ec45f727749b1
tophe <tophe@DESKTOP-EC8N5HK>
2017-12-16 09:54:15 -0600

    Added brim area

--- src/pixelHat/AniMaker.java
+++ src/pixelHat/AniMaker.java
@@ -11,16 +11,25 @@ public class AniMaker {
 	
 		BodyPixels cont = new BodyPixels();
 		Color [] c = {Color.BLACK, Color.RED, Color.GREEN, Color.BLUE, Color.WHITE};
-		cont.colorPalette = c;
-		
+		cont.colorPalette = c;		
 		cont.pixels[0][0] = 1;
 		cont.pixels[1][1] = 2;
 		cont.pixels[2][2] = 3;
 		cont.pixels[3][3] = 4;		
 		
+		BrimPixels brim = new BrimPixels();
+		brim.colorPalette = c;
+		brim.pixels[0][0] = 4;
+		brim.pixels[1][1] = 3;
+		brim.pixels[2][2] = 2;
+		brim.pixels[3][3] = 1;
+		
+		
+		
 		JFrame frame = new JFrame("AniMaker");
 			
 		frame.getContentPane().add(cont,BorderLayout.CENTER);
+		frame.getContentPane().add(brim,BorderLayout.SOUTH);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);		
