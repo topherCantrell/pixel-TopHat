94855afe95edb059fa62db48fe6c2745ac0914ea
tophe <tophe@DESKTOP-EC8N5HK>
2017-12-20 06:15:37 -0600

    working on save

--- src/pixelHat/AniMaker.java
+++ src/pixelHat/AniMaker.java
@@ -2,13 +2,32 @@ package pixelHat;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.io.IOException;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class AniMaker {
 	
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) throws IOException {
+		
+		FrameFile ff = new FrameFile("sampleSave.txt");
+		
+		JPanel controls = new JPanel(new FlowLayout());
+		JButton frev = new JButton("<<");
+		JButton rev = new JButton("<");
+		JButton forw = new JButton(">");
+		JButton fforw = new JButton(">>");
+		
+		JTextField frameNumber = new JTextField("");
+		
+		
+		controls.add(new JButton("Save"));
+		controls.add(new JButton("Cancel"));		
+		
 		BodyPixels cont = new BodyPixels();
 		Color [] c = {Color.BLACK, Color.RED, Color.GREEN, Color.BLUE, Color.WHITE};
 		cont.colorPalette = c;		
@@ -22,14 +41,19 @@ public class AniMaker {
 		brim.pixels[0][0] = 4;
 		brim.pixels[1][1] = 3;
 		brim.pixels[2][2] = 2;
-		brim.pixels[3][3] = 1;
+		brim.pixels[3][3] = 1;		
 		
+		JPanel pixels = new JPanel(new BorderLayout());
+		pixels.add(cont,BorderLayout.CENTER);
+		pixels.add(brim,BorderLayout.SOUTH);		
 		
-		
+		JPanel mainApp = new JPanel(new BorderLayout());
+		mainApp.add(pixels,BorderLayout.CENTER);
+		mainApp.add(controls,BorderLayout.SOUTH);
+			
 		JFrame frame = new JFrame("AniMaker");
 			
-		frame.getContentPane().add(cont,BorderLayout.CENTER);
-		frame.getContentPane().add(brim,BorderLayout.SOUTH);
+		frame.getContentPane().add(mainApp, BorderLayout.CENTER);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);		
