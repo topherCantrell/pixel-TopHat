14ee8b6dd10d320574c4aba4772fab5d3a5387eb
Christopher Cantrell <topherCantrell@gmail.com>
2018-11-26 16:15:27 -0600

    starting new movie

---
+++ FLL2018/orbit_movie.py
@@ -0,0 +1,12 @@
+'''
+Each row has one orbiter 8 to 24 long. Each created orbiter has a random wait to appear (as below)
+All orbiters travel right at speeds: 3,4, or 5.
+Orbiters scroll on and off.
+When orbiter dies, wait random 8 frames and create a new one, but only create 100 total orbiters.
+Continue moving until all orbiters are done
+
+Color pattern:
+
+1234 44444 5678
+
+'''
