7a90a9afcaa761c88e93d490d052e030192bc9a7
Christopher Cantrell <topherCantrell@gmail.com>
2018-11-28 14:51:58 -0600

    debug playing movies

--- lib/dev.py
+++ lib/dev.py
@@ -21,8 +21,26 @@ def show_frame(frame):
     _send_buffer(frame.get_binary(False))
     time.sleep(.1)
     
-def play_movie(filename,start_frame,end_frame=-1):
-    movie = make_sds.readMovie(filename)
-    # Send colors
-    # Send frames one by one with delay between
+def set_colors(colors):
+    col_buffer = [0]*1024    
+    pos = 0
+    for col in colors:
+        col_buffer[pos+0] = col & 0xFF
+        col_buffer[pos+1] = (col>>8) & 0xFF
+        col_buffer[pos+2] = (col>>16) & 0xFF
+        col_buffer[pos+3] = (col>>24) & 0xFF        
+        pos+=4
+    _ser.write(b'A')
+    _send_buffer(col_buffer)
+    _ser.write(b'B')
+    _send_buffer(col_buffer)
     
+def play_movie(filename,start_frame=0,end_frame=-1):
+    movie = make_sds.readMovie(filename)
+    set_colors(movie['colors'])
+    mfrs = movie['frames'][start_frame:end_frame]
+    for mfr in mfrs:
+        show_frame(mfr)
+        time.sleep(.5)
+    
+play_movie('../FLL2018/pacman.txt')
